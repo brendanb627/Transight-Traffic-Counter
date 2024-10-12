fafb31c36dafa988da3f992c8c9272e3c60b1402
Brendan Bessman <bdbessman@gmail.com>
2024-10-11 21:38:42 -0700

    added framework

---
+++ src/Screens/count-screen.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet, Text, Button } from "react-native";
+
+export const CountScreen = () => {
+  return (
+  <View>
+    <Button>BUTTON</Button>
+  </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ src/Screens/home-screen.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.text}>asdjsjdghjashdgjadgsh</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  box: {
+    width: 200,
+    height: 200,
+    backgroundColor: "lightblue",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    textAlign: "center",
+  }
+});
